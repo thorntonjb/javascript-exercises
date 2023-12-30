1b45e718b7564c2be8fa5c45df30dea527ef3dbc
john thornton <jb087011@gmail.com>
2023-12-30 14:25:48 -0500

    Complete third and fourth exercises
    Includes arrays, built-in array functions, strings, for loops

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...rest) {
+    let newArray = [];  
+    
+    array.forEach(element => {
+        if (!rest.includes(element)) {
+            newArray.push(element);
+        }
+    });
 
+    return newArray;
 };
 
 // Do not edit below this line
