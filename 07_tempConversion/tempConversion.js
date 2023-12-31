79ae747b0142135f974ed0cdd8702a0c7abf500e
john thornton <jb087011@gmail.com>
2023-12-30 21:17:27 -0500

    Complete seventh and eighth exercises
    Includes for loops, arrays, operations, conditionals

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+    if (temperature == 32)
+      return 0;
+
+    temperature = (temperature - 32) * (5/9);
+    return Math.round(temperature * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+    if (temperature == 0)
+      return 32;
+
+    temperature = (temperature * (9/5) + 32);
+    return Math.round(temperature * 10) / 10;
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,62 @@
-const add = function() {
-	
+const add = function(...args) {
+    let total = 0;
+    for (const arg of args)
+      total += arg;
+
+    return total;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second, ...args) {
+    let total = first - second;
+    for (const arg of args) {
+      total -= arg
+    }
+
+    return total;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+    let total = 0;
+    for (let i = 0; i < array.length; i++) {
+      total += array[i];
+    }
+
+    return total;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let total = 1;
+  for (let i = 0; i < array.length; i++) {
+    total *= array[i];
+  }
 
+    return total;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	  let total = base;
+    for (let i = 0; i < power - 1; i++)
+    {
+      total *= base;
+    }
+
+    return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(base) {
+    if (base == 0)
+      return 1;
+    if (base == 1)
+      return 1;
+
+    let array = [];
+    let total = 1;
+    for (let i = 1; i < base + 1; i++) {
+      array[i] = i;
+      total *= i;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
