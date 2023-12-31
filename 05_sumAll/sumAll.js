f34c239a957b13f0a4120a801b747225139830f9
john thornton <jb087011@gmail.com>
2023-12-30 20:33:14 -0500

    Complete fifth and sixth exercise
    Using for loops, built-in array methods, arrays, conditionals

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    if (first < 0 || second < 0)
+        return "ERROR";
+    if (!(typeof first === 'number') || !(typeof second === 'number'))
+        return "ERROR";
 
+    let arrayLength = Math.abs(second - first) + 1;
+    let array = [arrayLength];
+    let firstElement;
+
+    if (first < second)
+        firstElement = first;
+    if (second < first)
+        firstElement = second;
+
+    let sum = 0;
+
+    for(let i = 0; i < arrayLength; i++)
+        {
+            array[i] = firstElement;
+            firstElement++;
+        }
+
+    array.forEach(element => {
+        sum += element;
+    });
+
+    return sum;
 };
 
 // Do not edit below this line
