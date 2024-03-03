f53f4430dc517b16a509ac1384753841ccf006ce
john thornton <jb087011@gmail.com>
2024-03-03 18:30:18 -0500

    Pass Ninth and Tenth Tests
    Palindromes and Fibonacci sequence.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,7 +1,23 @@
 const palindromes = function (word) {
+    let lastLetter = word.length - 1;
+    let state = false;
+    word = word.replaceAll(" ", "");
+
     for (let i = 0; i < word.length; i++) {
-        
+      if (word[i] == word[lastLetter])
+      {
+        console.log("word[i]: " + word[i]);
+        console.log("word[lastLetter]: " + word[lastLetter]);
+        state = true;
+        lastLetter--;
+      }
+      else
+      {
+        state = false;
+      }
     }
+    console.log(state);
+    return state;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+  if (member < 0)
+    return "OOPS";
 
+  let n1 = 0, n2 = 1, nextTerm;
+
+  for (let i = 1; i <= member; i++) {
+      console.log(n1);
+      nextTerm = n1 + n2;
+      n1 = n2;
+      n2 = nextTerm;
+  }
+
+  return n1;
 };
 
 // Do not edit below this line
